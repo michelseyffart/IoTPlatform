5e4ed464e17dd0c4f88255b01ef191e4cbfc4462
Michel Seyffart <michel.seyffart@eonerc.rwth-aachen.de>
2024-01-12 14:45:11 +0100

    update collector

--- data_collection/collector.py
+++ data_collection/collector.py
@@ -7,17 +7,17 @@ import logs.create_logger as logs
 import logging
 
 
-class Collector:
+class MQTTCollector:
     def __init__(self):
-        self.log = logs.get_logger(filename="run.log", name="collector", consolelevel=logging.INFO)
-        folder = Path(__file__).parent.joinpath("rawdata").resolve()
+        self.log = logs.get_logger(filename="run.log", name="MQTTCollector", consolelevel=logging.INFO)
+        folder = Path(__file__).parent.joinpath("rawdata_MQTT").resolve()
         time_now = datetime.datetime.now().strftime("%Y-%m-%d-%H-%M-%S")
         self.file_name = folder.joinpath(f"{time_now}.p")
         self.mqttc = mqtt.Client()
         self.mqttc.on_message = self.on_message
         self.mqttc.connect("137.226.248.250")
         self.mqttc.subscribe("data/#")
-        self.log.info("Created data collector")
+        self.log.info("Created MQTT data collector")
 
     def start(self):
         self.mqttc.loop_start()
@@ -26,17 +26,33 @@ class Collector:
     def on_message(self, client, userdata, message):
         topic = message.topic
         payload = message.payload.decode("utf-8")
-        data_to_save = [topic, payload]
+        time_now = datetime.datetime.now()
+        data_to_save = [topic, payload, time_now]
         with open(self.file_name, "ab") as f:
-            pickle.dump(data_to_save, file=f)
+            pickle.dump(data_to_save, file=f,)
 
     def stop(self):
         self.mqttc.loop_stop()
         self.log.info("Data collector stopped")
 
 
+class PythonCollector:
+    def __init__(self):
+        self.log = logs.get_logger(filename="run.log", name="PythonCollector", consolelevel=logging.INFO)
+        folder = Path(__file__).parent.joinpath("rawdata_python").resolve()
+        time_now = datetime.datetime.now().strftime("%Y-%m-%d-%H-%M-%S")
+        self.file_name = folder.joinpath(f"{time_now}.p")
+        self.log.info("Created Python data collector")
+
+    def save_data(self, data):
+        time_now = datetime.datetime.now()
+        data_to_save = [data, time_now]
+        with open(self.file_name, "ab") as f:
+            pickle.dump(data_to_save, file=f)
+
+
 if __name__ == "__main__":
-    c = Collector()
+    c = MQTTCollector()
     c.start()
     time.sleep(30)
     c.stop()
