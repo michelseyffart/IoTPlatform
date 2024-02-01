9067052e7b14e3e370b34f6fe422d105347f2acc
Michel Seyffart <michel.seyffart@eonerc.rwth-aachen.de>
2024-02-01 18:12:50 +0100

    update WT opti calculation

--- building_data_generation/wind_turbine_data.py
+++ building_data_generation/wind_turbine_data.py
@@ -1,4 +1,4 @@
-import pickle
+import json
 
 import classes.datahandler as datahandler
 
@@ -17,21 +17,31 @@ def generate_data():
     return districtData.centralDevices["renewableGeneration"]["centralWT"]
 
 
-def calculate_opti_results(data):
+def calculate_opti_results(data, scenario):
     opti_res = dict()
     for n_opt in range(8760):
         opti_res[n_opt] = f"surplus:{data[n_opt]}, demand:0, res_soc_tes:0, res_soc_bat:0"
-    return opti_res
-
-
-def save_opti_results(opti_res: dict, scenario: str):
     dir_path = Path(path_openhab_container_scripts.joinpath("data", "opti_results", scenario))
     dir_path.mkdir(parents=True, exist_ok=True)
-    with open(dir_path.joinpath("WT.p"), "wb") as f:
-        pickle.dump(opti_res, f)
+    with open(dir_path.joinpath("WT.json"), "w") as f:
+        json.dump(opti_res, f, indent=2)
+
+
+def calculate_opti_results_for_months(data, months: list, scenario):
+    dir_path = Path(path_openhab_container_scripts.joinpath("data", "opti_results", scenario))
+    for month in months:
+        month_dir_path = dir_path.joinpath(f"month_{month}")
+        month_dir_path.mkdir(parents=True, exist_ok=True)
+
+        opti_res = dict()
+        for n_opt in range(8760):
+            opti_res[n_opt] = f"surplus:{data[n_opt]}, demand:0, res_soc_tes:0, res_soc_bat:0"
+        with open(month_dir_path.joinpath("WT.json"), "w") as f:
+            json.dump(opti_res, f, indent=2)
 
 
 if __name__ == "__main__":
     elec_production = generate_data()
-    opti_results = calculate_opti_results(data=elec_production)
-    save_opti_results(opti_res=opti_results, scenario="half_pv")
+    calculate_opti_results_for_months(data=elec_production, scenario="40_1", months=[1, 4, 7])
+    calculate_opti_results_for_months(data=elec_production, scenario="40_2", months=[1, 4, 7])
+    calculate_opti_results_for_months(data=elec_production, scenario="40_3", months=[1, 4, 7])
