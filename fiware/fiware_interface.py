4bb5b1f11fa5a0ef0d5d71fc1214e65a7ee9973d
Michel Seyffart <michel.seyffart@eonerc.rwth-aachen.de>
2024-02-13 16:51:24 +0100

    update fiware interface to handle connection loss

--- fiware/fiware_interface.py
+++ fiware/fiware_interface.py
@@ -5,7 +5,7 @@ from market.auction_iteration import AuctionIteration
 from market.public_info import PublicInfo
 import fiware.setup as setup
 from requests import Session
-import requests.exceptions
+from requests.exceptions import ReadTimeout, HTTPError, ConnectionError
 import logs.create_logger as logs
 import logging
 import fiware.config.config as fiware_config
@@ -23,7 +23,14 @@ class FiwareInterface:
         self.log = logs.get_logger(filename="fiware_interface.log", name="fiware interface", consolelevel=logging.INFO)
 
     def get_bid_entities(self):
-        return self.cbc.get_entity_list(type_pattern="Bid")
+        try:
+            return self.cbc.get_entity_list(type_pattern="Bid")
+        except ReadTimeout as e:
+            self.log.error(f"Could not get bid entities. {e}")
+            return list()
+        except ConnectionError as e:
+            self.log.error(f"Could not get bid entities. {e}")
+            return list()
 
     def update_transaction(self, transaction_info: TransactionInfo, failed_previously: bool = False):
         try:
@@ -36,20 +43,25 @@ class FiwareInterface:
             self.cbc.update_attribute_value(entity_id=f"Transaction:{transaction_info.id}",
                                             attr_name="TransactionQuantity",
                                             value=transaction_info.quantity_res)
-        except requests.exceptions.HTTPError as e:
+        except HTTPError as e:
             if not failed_previously:
                 setup.post_entity_transaction(cbc=self.cbc, building=transaction_info.id)
                 self.update_transaction(transaction_info=transaction_info, failed_previously=True)
             else:
                 self.log.error(f"Could not post transaction. {e}")
+        except ConnectionError as e:
+            self.log.error(f"Could not post transaction. {e}")
 
     def reset_bid(self, bid: Bid):
-        self.cbc.update_attribute_value(entity_id=f"Bid:{bid.id}",
-                                        attr_name="BidPrice",
-                                        value=0)
-        self.cbc.update_attribute_value(entity_id=f"Bid:{bid.id}",
-                                        attr_name="BidQuantity",
-                                        value=0)
+        try:
+            self.cbc.update_attribute_value(entity_id=f"Bid:{bid.id}",
+                                            attr_name="BidPrice",
+                                            value=0)
+            self.cbc.update_attribute_value(entity_id=f"Bid:{bid.id}",
+                                            attr_name="BidQuantity",
+                                            value=0)
+        except ConnectionError as e:
+            self.log.error(f"Could not reset bid {bid.id}. {e}")
 
     def update_auction_iteration(self, auction_iteration: AuctionIteration, failed_previously: bool = False):
         try:
@@ -62,12 +74,14 @@ class FiwareInterface:
             self.cbc.update_attribute_value(entity_id=f"Auction_Iteration:{auction_iteration.id}",
                                             attr_name="AuctionIterationQuantity",
                                             value=auction_iteration.quantity)
-        except requests.exceptions.HTTPError as e:
+        except HTTPError as e:
             if not failed_previously:
                 setup.post_entity_auction_iteration(building=auction_iteration.id, cbc=self.cbc)
                 self.update_auction_iteration(auction_iteration=auction_iteration, failed_previously=True)
             else:
                 self.log.error(f"Could not post auction iteration. {e}")
+        except ConnectionError as e:
+            self.log.error(f"Could not post auction iteration. {e}")
 
     def update_public_info(self, public_info: PublicInfo, failed_previously: bool = False):
         try:
@@ -77,14 +91,19 @@ class FiwareInterface:
             self.cbc.update_attribute_value(entity_id="Public_Info",
                                             attr_name="EquilibriumQuantity",
                                             value=public_info.equilibrium_quantity)
-        except requests.exceptions.HTTPError as e:
+        except HTTPError as e:
             if not failed_previously:
                 setup.post_entity_public_info(cbc=self.cbc)
                 self.update_public_info(public_info=public_info, failed_previously=True)
             else:
                 self.log.error(f"Could not post public info. {e}")
+        except ConnectionError as e:
+            self.log.error(f"Could not post public info. {e}")
 
     def update_bid_quantity(self, bid: Bid):
-        self.cbc.update_attribute_value(entity_id=f"Bid:{bid.id}",
-                                        attr_name="BidQuantity",
-                                        value=bid.quantity)
+        try:
+            self.cbc.update_attribute_value(entity_id=f"Bid:{bid.id}",
+                                            attr_name="BidQuantity",
+                                            value=bid.quantity)
+        except ConnectionError as e:
+            self.log.error(f"Could not update bid quantity {bid.id}. {e}")
